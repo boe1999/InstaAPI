792de2417ede13b93cc0df1dc8748c58a32d637b
Boe <boerio99@gmail.com>
2023-03-21 12:26:12 +0100

    added comments to code

--- InstaAPI.py
+++ InstaAPI.py
@@ -3,33 +3,38 @@ import instaloader
 
 app = Flask(__name__)
 
+#use Set to not permit to use ripetitive data
 followersSet = set()
 followeesSet = set()
 
 class Insta_info:
 
+    #initialize class instaloader with username, loader and profile
     def __init__(self, username):
 
         self.username = username
         self.loader = instaloader.Instaloader()
         self.profile = instaloader.Profile.from_username(self.loader.context,self.username)
 
-
+    #define the function login to Log in with username and password
     def Login(self, username, password):
 
         login = self.loader.login(username, password)
         return login
-
+    
+    #define the function to update Set of followers
     def get_my_followers(self):
 
         for followers in self.profile.get_followers():
             followersSet.add(followers.username)
 
+    #define the function to upadate Set of followees
     def get_my_followees(self):
 
         for followees in self.profile.get_followees():
             followeesSet.add(followees.username)
 
+    #define the function that return Set of unFollowers
     def get_my_unfollowers(self):
         followers = set(followersSet)
         followees = set(followeesSet)
@@ -37,9 +42,12 @@ class Insta_info:
 
 @app.route('/')
 def main():
+    #retrieve username and password from url (GET)
     username = request.args.get('user')
     password = request.args.get('passw')
+    #initialize class insta_info
     insta_info = Insta_info(username)
+    #try to login and call functions otherwise call exception
     try:
         insta_info.Login(username, password)
         insta_info.get_my_followers()
